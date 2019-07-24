4aaa8a2d18ac84c4f4ac8100a97e222ef6bbb6d6
Sai Sri Mourya Gudladona <mourya.gudladona@gmail.com>
2019-07-24 17:49:21 -0400

    Delete generate_input.py

--- Static_Voting_Protocol/src/generate_input.py
+++
@@ -1,9 +0,0 @@
-import random
-
-maxMsg = raw_input('Enter Max Message: ')
-print "Max Messages: {}".format(maxMsg)
-
-maxNodes = raw_input('Enter Max Nodes: ')
-print "Max Nodes: {}".format(maxNodes)
-print "----------------------------------------------------"
-
